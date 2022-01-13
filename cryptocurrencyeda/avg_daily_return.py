f69072e0ff37876dea85b4f34c736b150c718338
alexYinanGu0 <75028436+alexYinanGu0@users.noreply.github.com>
2022-01-13 02:09:17 -0800

    Change the file from .ipynb to .py

---
+++ cryptocurrencyeda/avg_daily_return.py
@@ -0,0 +1,20 @@
+def avg_daily_return():
+    """
+    Outputs the average daily return of the inputted cryptocurrency price.
+    
+    Parameters
+    ----------
+    name : array-like
+        Inputted cryptocurrency price.
+    
+    Returns
+    -------
+    float
+        Average daily return of the cryptocurrency.
+    
+    Examples
+    --------
+    >>>
+    >>>
+    """
+
