29c877af08ddf886b23c6d0ece6743891c237c96
Cici Du <cicidhz@gmail.com>
2022-01-21 14:00:29 -0800

    docs: add documentation for test function

--- tests/test_plot_price.py
+++ tests/test_plot_price.py
@@ -3,15 +3,18 @@ import numpy as np
 import altair as alt
 import pandas as pd
 
-helper_data = pd.DataFrame({
-    'Name': np.array(['Bitcoin', 'Bitcoin', 'Bitcoin', 'Bitcoin' ,'Bitcoin']),
-    'Date': np.array(['2021-01-01', '2021-01-02', '2021-01-03', '2021-01-04', '2021-01-05']),
-    'Close': np.array([29374.15189, 32127.26794, 32782.02447, 31971.91352, 33992.42934])
-})
+def test_plot_price(helper_plot):
+    """
+    Test the plot_price function returns a plot with the correct attributes.
+    """
+    helper_data = pd.DataFrame({
+        'Name': np.array(['Bitcoin', 'Bitcoin', 'Bitcoin', 'Bitcoin' ,'Bitcoin']),
+        'Date': np.array(['2021-01-01', '2021-01-02', '2021-01-03', '2021-01-04', '2021-01-05']),
+        'Close': np.array([29374.15189, 32127.26794, 32782.02447, 31971.91352, 33992.42934])
+    })
 
-helper_plot = CryptoCurrencyEDA.plot_price(helper_data)
+    helper_plot = CryptoCurrencyEDA.plot_price(helper_data)
 
-def test_plot_price():
     assert helper_plot.encoding.x.field == 'Date', 'x_axis should be mapped to the x axis'
     assert helper_plot.encoding.y.field == 'Close', 'y_axis should be mapped to the y axis'
     assert helper_plot.mark == 'line', 'mark should be a line'
