ce17a953a3e3ea089293556e8c179fa1c91c4b14
Cici Du <cicidhz@gmail.com>
2022-01-21 19:37:56 -0800

    Update details

--- tests/test_CryptocurrencyEDA.py
+++
@@ -1,1 +0,0 @@
-from cryptocurrencyeda import cryptocurrencyeda
--- tests/test_plot_price.py
+++ tests/test_plot_price.py
@@ -1,4 +1,5 @@
-from CryptocurrencyEDA import CryptoCurrencyEDA
+from cryptocurrencyeda.plot_price import plot_price
+
 import numpy as np
 import altair as alt
 import pandas as pd
@@ -13,7 +14,7 @@ def test_plot_price(helper_plot):
         'Close': np.array([29374.15189, 32127.26794, 32782.02447, 31971.91352, 33992.42934])
     })
 
-    helper_plot = CryptoCurrencyEDA.plot_price(helper_data)
+    helper_plot = plot_price(helper_data)
 
     assert helper_plot.encoding.x.field == 'Date', 'x_axis should be mapped to the x axis'
     assert helper_plot.encoding.y.field == 'Close', 'y_axis should be mapped to the y axis'
